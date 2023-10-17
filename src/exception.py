1345e1363eb4e2165bc3d0801da1b0497d831a0d
Yaxuan Yang <yaxuan.yang2007@gmail.com>
2023-10-17 12:57:52 -0700

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -17,10 +17,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-# if __name__=='__main__':
+if __name__=='__main__':
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info('Divide by zero')
-#         raise CustomException(e,sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divide by zero')
+        raise CustomException(e,sys)
